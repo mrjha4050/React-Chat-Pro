9c1229f0d134d485a72dc845f03d087f5094a1d5
Naman Jha <108355610+mrjha4050@users.noreply.github.com>
2023-12-29 00:53:12 +0530

    Add files via upload

---
+++ React-Chat-Pro/frontend/src/ChatsPage.jsx
@@ -0,0 +1,21 @@
+// const ChatsPage = () => {
+//     return <>chats...</>
+// }
+
+// export default ChatsPage
+
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div className="background">
+      <PrettyChatWindow
+        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
+        username={props.user.username}
+        secret={props.user.secret}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
