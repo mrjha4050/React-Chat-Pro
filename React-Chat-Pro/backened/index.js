07edef1d1d47d3cbfb3b65ce7769f97e31607ede
Naman Jha <108355610+mrjha4050@users.noreply.github.com>
2023-12-29 01:43:19 +0530

    Update index.js

--- React-Chat-Pro/backened/index.js
+++ React-Chat-Pro/backened/index.js
@@ -1,3 +1,31 @@
+// const express = require("express");
+// const cors = require("cors");
+// const { default: axios } = require("axios");
+
+// const app = express();
+// app.use(express.json());
+// app.use(cors({ origin: true }));
+
+// app.post("/authenticate", async (req, res) => {
+//   const { username } = req.body;
+//   try {
+//     const r = await axios.put(
+//       "https://api.chatengine.io/users/",
+//       { username: username, secret: username, first_name: username },
+//       { headers: { "Private-Key": "YOUR_PRIVATE_KEY" } }
+//     );
+//     return res.status(r.status).json(r.data);
+//   } catch (e) {
+//     return res.status(e.response.status).json(e.response.data);
+//   }
+// });
+
+// app.listen(3001); 
+
+// Above code can be use to for without using environment
+
+// Below code can be used for usign env 
+
 const express = require("express");
 const cors = require("cors");
 const { default: axios } = require("axios");
@@ -6,13 +34,21 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
+// Fetch the API key from environment variables
+const privateKey = process.env.API_PRIVATE_KEY;
+
+if (!privateKey) {
+  console.error("API_PRIVATE_KEY is not set in environment variables.");
+  process.exit(1);
+}
+
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
   try {
     const r = await axios.put(
       "https://api.chatengine.io/users/",
       { username: username, secret: username, first_name: username },
-      { headers: { "Private-Key": "YOUR_PRIVATE_KEY" } }
+      { headers: { "Private-Key": privateKey } }
     );
     return res.status(r.status).json(r.data);
   } catch (e) {
@@ -20,4 +56,10 @@ app.post("/authenticate", async (req, res) => {
   }
 });
 
-app.listen(3001); 
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
